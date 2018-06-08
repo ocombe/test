80e1770320e51f8ad8cfb5e49b883a770e1af93f
Fabian Wiles <fabianwiles@live.com>
2018-06-08 21:48:44 +1200

    Update size.js

--- size.js
+++ size.js
@@ -14,14 +14,7 @@ storeArtifacts
 storeArtifacts
 
 
-branch
-storeArtifacts"https://circleci.com/api/v1.1/project/github/ocombe/test/3/artifacts"
-storeArtifacts
-111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
-111111111111111111111111111
-111111111111111111111111111"https://circleci.com/api/v1.1/project/github/ocombe/test/3/artifacts"
-"https://circleci.com/api/v1.1/project/github/ocombe/test/3/artifacts"
-"https://circleci.com/api/v1.1/project/github/ocombe/test/3/artifacts"
+branchproject/github/ocombe/test/3/artifacts"
 response
       // this.logWarn({context}, `Update status for unknown PR, ignored. Head sha == ${context.payload.sha}, repository == ${context.payload.repository.id}`);
 111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
